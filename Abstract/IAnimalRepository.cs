4ebad7205f8bdb62347e3e9cd92ec21b1878fb49
A09F\filip <filippvoloshin@gmail.com>
2017-05-23 22:25:21 +0300

    added classes whith repository logic

--- Abstract/IAnimalRepository.cs
+++ Abstract/IAnimalRepository.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ZooImitation.Abstract
+﻿namespace ZooImitation.Abstract
 {
-    interface IAnimalRepository
+    public interface IAnimalRepository
     {
+        void Add(IAnimal animal, string name);
+        void Feed(string name);
+        void Cure(string name);
+        void Kick();
+        int Count { get; }
     }
 }
