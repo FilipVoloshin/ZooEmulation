d0491117930630eef0de9453f84bd13c22f47e10
A09F\filip <filippvoloshin@gmail.com>
2017-05-25 00:52:33 +0300

    Created user iteration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ZooImitation.AnimalTypes;
 
 namespace ZooImitation
 {
@@ -8,7 +9,65 @@ namespace ZooImitation
         {
             try
             {
-                
+                var animals = new AnimalRepository();
+                var zoo = new Zoo(animals);
+                do
+                {
+                    Console.WriteLine(new string('*', 22));
+                    Console.WriteLine("Choose action:\n1. Add animal \n2. Feed animal\n3. Cure animal \n4. Remove dead animals");
+                    Console.WriteLine(new string('*', 22));
+
+                    int variant = Convert.ToInt32(Console.ReadLine());
+
+                    switch (variant)
+                    {
+                        case 1:
+                            Console.WriteLine($"Choose animal\'s type:\n " +
+                                $"1.Bear\n 2.Elephant\n 3.Fox\n 4.Lion\n 5.Tiger\n 6.Wolf");
+                            int animalType = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter pets name:\t");
+                            var name = Console.ReadLine();
+                            switch (animalType)
+                            {
+                                case 1:
+                                    animals.Add(new Bear(), name);
+                                    break;
+                                case 2:
+                                    animals.Add(new Elephant(), name);
+                                    break;
+                                case 3:
+                                    animals.Add(new Fox(), name);
+                                    break;
+                                case 4:
+                                    animals.Add(new Lion(), name);
+                                    break;
+                                case 5:
+                                    animals.Add(new Tiger(), name);
+                                    break;
+                                case 6:
+                                    animals.Add(new Wolf(), name);
+                                    break;
+                                default:
+                                    Console.WriteLine("Unknown type of animal!");
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter animal\'s name which you want to feed:");
+                            name = Console.ReadLine();
+                            animals.Feed(name);
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter animal\'s name which you want to cure:");
+                            name = Console.ReadLine();
+                            animals.Cure(name);
+                            break;
+                        case 4:
+                            animals.Kick();
+                            break;
+                    }
+                }
+                while (true);  
             }
             catch (ApplicationException ex)
             {
