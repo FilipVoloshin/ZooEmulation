0135d83ecb4818118a84887b2dde3c003fbe92b3
A09F\filip <filippvoloshin@gmail.com>
2017-05-24 13:04:19 +0300

    added summary for extension method

--- Helpers/Display.cs
+++ Helpers/Display.cs
@@ -4,6 +4,10 @@ namespace ZooImitation
 {
     public static class Display
     {
+        /// <summary>
+        /// Logs string messages in Console
+        /// </summary>
+        /// <param name="message">Message to log in console</param>
         public static void ConsoleWrite(this string message)
         {
             var wordsCount = message.Length + 3;
