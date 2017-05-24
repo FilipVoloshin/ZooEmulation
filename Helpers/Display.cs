78c614912476468debb05d4f8c36cf3ce3538c53
A09F\filip <filippvoloshin@gmail.com>
2017-05-24 12:01:14 +0300

    Added extesion method with ability to log in the console

---
+++ Helpers/Display.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZooImitation
+{
+    public static class Display
+    {
+        public static void ConsoleWrite(this string message)
+        {
+            var wordsCount = message.Length + 3;
+            Console.WriteLine(new string('-',wordsCount));
+            Console.WriteLine($" - {message}");
+        }
+    }
+}
