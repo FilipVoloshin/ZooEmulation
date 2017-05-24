cac342e1f856b6417633c4a399e2635782a7b171
A09F\filip <filippvoloshin@gmail.com>
2017-05-24 12:54:12 +0300

    Added ability to log every user act in console

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZooImitation.AnimalTypes;
 
 namespace ZooImitation
 {
@@ -10,6 +11,27 @@ namespace ZooImitation
     {
         static void Main(string[] args)
         {
+            AnimalRepository animalRepo = new AnimalRepository();
+            animalRepo.Add(new Bear(), "Filip");
+            animalRepo.Add(new Elephant(), "Larisa");
+            animalRepo.Add(new Fox(), "Larisa");
+            animalRepo.Add(new Lion(), "Filip");
+            animalRepo.Add(new Wolf(), "Filip");
+            animalRepo.Add(new Tiger(), "Filip");
+            animalRepo.Cure("Filip");
+            animalRepo.Feed("Filip");
+
+            animalRepo.Animals[3].CurrentHealth = 2;
+            animalRepo.Animals[4].CurrentHealth = 1;
+            animalRepo.Animals[5].CurrentHealth = 3;
+
+            animalRepo.ShowAnimals();
+
+            animalRepo.Cure("Filip");
+
+            animalRepo.ShowAnimals();
+
+            Console.ReadLine();
         }
     }
 }
