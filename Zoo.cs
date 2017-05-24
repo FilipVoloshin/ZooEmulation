40fd7db018ca2d2d0a1d12a833040fafcb6361af
A09F\filip <filippvoloshin@gmail.com>
2017-05-24 18:37:17 +0300

    added Zoo class

---
+++ Zoo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooImitation.Abstract;
+
+namespace ZooImitation
+{
+    class Zoo
+    {
+        private IAnimalRepository _animals;
+        public Zoo(IAnimalRepository animals)
+        {
+            _animals = animals;
+        }
+
+        public int GetRandomNumber()
+        {
+            var random = new Random();
+            var animalsCount = _animals.Count;
+            int randomValue;
+            if (animalsCount > 0)
+            {
+                randomValue = random.Next(0, animalsCount);
+                return randomValue;
+            }
+            else
+                throw new ArgumentNullException("Zero animals count!", "There are no animals in Zoo!");
+        }
+    }
+}
